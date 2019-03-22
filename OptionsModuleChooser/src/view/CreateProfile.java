5d160ce710f62ec89896940838755d24a59ff291
『　　』 <faizal_10@live.co.uk>
2019-03-22 23:48:25 +0000

    WIP CreateProfile tab

---
+++ OptionsModuleChooser/src/view/CreateProfile.java
@@ -0,0 +1,71 @@
+package view;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import model.Course;
+
+public class CreateProfile extends GridPane{
+
+	private ComboBox<Course> cboCourses;
+	private TextField txtPNumber, txtFirstName, txtSurname, txtEMail, txtSubmissionDate;
+	private Button btnCreateProfile;
+
+	public CreateProfile() {
+		//TODO: styling
+		this.setPadding(new Insets(80, 80, 80, 80));
+		this.setVgap(15);
+		this.setHgap(20);
+		this.setAlignment(Pos.CENTER);
+
+		// Create labels
+		Label lblCourses = new Label("Select course: ");
+		Label lblPNumber = new Label("P Number: ");
+		Label lblFirstName = new Label("First name: ");
+		Label lblSurname = new Label("Surname: ");
+		Label lblEmail = new Label("Email address");
+		Label lblDate = new Label("Submission date");
+
+		// Create combo box
+		cboCourses = new ComboBox<Course>();
+
+		// Setup text fields
+		txtPNumber = new TextField();
+		txtFirstName = new TextField();
+		txtSurname = new TextField();
+		txtEMail = new TextField();
+		txtSubmissionDate = new TextField();
+
+		// Initialise submit button
+		btnCreateProfile = new Button("Create Profile");
+
+		//add controls and labels to container
+		this.add(lblCourses, 0, 0);
+		this.add(cboCourses, 1, 0);
+
+		this.add(lblPNumber, 0, 1);
+		this.add(txtPNumber, 1, 1);
+
+		this.add(lblFirstName, 0, 2);
+		this.add(txtFirstName, 1, 2);
+
+		this.add(lblSurname, 0, 3);
+		this.add(txtSurname, 1, 3);
+
+		this.add(lblEmail, 0, 4);
+		this.add(txtEMail, 1, 4);
+
+		this.add(lblDate, 0, 5);
+		this.add(txtSubmissionDate, 1, 5);
+
+		this.add(new HBox(), 0, 6);
+		this.add(btnCreateProfile, 1, 6);
+	}
+
+}
--- OptionsModuleChooser/src/view/OptionsModuleChooserRootPane.java
+++ OptionsModuleChooser/src/view/OptionsModuleChooserRootPane.java
@@ -6,13 +6,17 @@ import javafx.scene.layout.BorderPane;
 
 //You may change this class to extend another type if you wish
 public class OptionsModuleChooserRootPane extends TabPane {
+	private CreateProfile createprofile;
 	TabPane tabPane = new TabPane();
 	Tab tab = new Tab();
-	
+
+
 	public OptionsModuleChooserRootPane() {
-	Tab t1 = new Tab("Create Profile");
-	Tab t2 = new Tab("Select Modules");
-	Tab t3 = new Tab("Overview Selection");
-	this.getTabs().addAll(t1, t2, t3);
+		createprofile = new CreateProfile();
+
+		Tab t1 = new Tab("Create Profile", createprofile);
+		Tab t2 = new Tab("Select Modules");
+		Tab t3 = new Tab("Overview Selection");
+		this.getTabs().addAll(t1, t2, t3);
 	}
 }
